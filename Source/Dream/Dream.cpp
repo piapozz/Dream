160628346ac4a6e48daee41d5ad24f8832ed63bd
Piapozz <mcd2376090@stu.o-hara.ac.jp>
2026-08-16 22:37:35 +0900

    いけるはずだ！

---
+++ Source/Dream/Dream.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Dream.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Dream, "Dream" );
+
+DEFINE_LOG_CATEGORY(LogDream)
